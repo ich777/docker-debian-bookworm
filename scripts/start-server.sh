2c6a0474de3a6adac4420b5b0a2119b13d8232aa
ich777 <christophhummer@gmail.com>
2023-10-11 13:31:50 +0200

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -30,10 +30,6 @@ fi
 if [ ! -d ${DATA_DIR}/.local/share/applications ]; then
 	mkdir -p ${DATA_DIR}/.local/share/applications
 fi
-if [ ! -f ${DATA_DIR}/.local/share/applications/x11vnc.desktop  ]; then
-	cp /usr/share/applications/x11vnc.desktop ${DATA_DIR}/.local/share/applications/
-	echo "Hidden=true" >> ${DATA_DIR}/.local/share/applications/x11vnc.desktop
-fi
 
 echo "---Checking for old logfiles---"
 find ${DATA_DIR}/.logs -name "XvfbLog.*" -exec rm -f {} \;
