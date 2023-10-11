d1ecc274774496d4a8240a3d07ae57ee8b9c0593
ich777 <christophhummer@gmail.com>
2023-10-11 13:23:36 +0200

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -5,6 +5,36 @@ export XAUTHORITY=${DATA_DIR}/.Xauthority
 export LANGUAGE="$LOCALE_USR"
 export LANG="$LOCALE_USR"
 
+echo "---Preparing Server---"
+if [ ! -d ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/ ]; then
+	mkdir -p ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+if [ ! -f ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml ]; then
+	cp /tmp/config/xfce4-desktop.xml ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+if [ ! -f ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml ]; then
+	cp /tmp/config/xfce4-panel.xml ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+if [ ! -f ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml ]; then
+	cp /tmp/config/xsettings.xml ${DATA_DIR}/.config/xfce4/xfconf/xfce-perchannel-xml/
+fi
+if [ ! -d ${DATA_DIR}/.config/xfce4/terminal ]; then
+	mkdir -p ${DATA_DIR}/.config/xfce4/terminal
+fi
+if [ ! -f ${DATA_DIR}/.config/xfce4/terminal/terminalrc ]; then
+	cp /tmp/config/terminalrc ${DATA_DIR}/.config/xfce4/terminal/terminalrc
+fi
+if [ ! -d ${DATA_DIR}/.logs ]; then
+	mkdir ${DATA_DIR}/.logs
+fi
+if [ ! -d ${DATA_DIR}/.local/share/applications ]; then
+	mkdir -p ${DATA_DIR}/.local/share/applications
+fi
+if [ ! -f ${DATA_DIR}/.local/share/applications/x11vnc.desktop  ]; then
+	cp /usr/share/applications/x11vnc.desktop ${DATA_DIR}/.local/share/applications/
+	echo "Hidden=true" >> ${DATA_DIR}/.local/share/applications/x11vnc.desktop
+fi
+
 echo "---Checking for old logfiles---"
 find ${DATA_DIR}/.logs -name "XvfbLog.*" -exec rm -f {} \;
 find ${DATA_DIR}/.logs -name "x11vncLog.*" -exec rm -f {} \;
@@ -19,14 +49,14 @@ fi
 screen -wipe 2&>/dev/null
 find /var/run/dbus -name "pid" -exec rm -f {} \;
 
-#echo "---Starting dbus service---"
-#if dbus-daemon --config-file=/usr/share/dbus-1/system.conf ; then
-#	echo "---dbus service started---"
-#else
-#	echo "---Couldn't start dbus service---"
-#	sleep infinity
-#fi
-#sleep 2
+echo "---Starting dbus service---"
+if dbus-daemon --config-file=/usr/share/dbus-1/system.conf ; then
+	echo "---dbus service started---"
+else
+	echo "---Couldn't start dbus service---"
+	sleep infinity
+fi
+sleep 2
 
 echo "---Starting TurboVNC server---"
 vncserver -geometry ${CUSTOM_RES_W}x${CUSTOM_RES_H} -depth ${CUSTOM_DEPTH} :99 -rfbport ${RFB_PORT} -noxstartup ${TURBOVNC_PARAMS} 2>/dev/null
